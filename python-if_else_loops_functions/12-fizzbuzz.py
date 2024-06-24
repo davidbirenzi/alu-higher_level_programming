584c8707b1c3ae53ac4b90d04d2e2dc23cfc3f99
davidbirenzi <d.birenzi@alustudent.com>
2024-06-24 14:20:37 -0700

    FizzBuzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    result = []
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            result.append("FizzBuzz")
+            print("FizzBuzz", end=" ")
         elif i % 3 == 0:
-            result.append("Fizz")
+            print("Fizz", end=" ")
         elif i % 5 == 0:
-            result.append("Buzz")
+            print("Buzz", end=" ")
         else:
-            result.append(str(i))
-    print(" ".join(result))
-
-# Example usage:
-if __name__ == "__main__":
-    fizzbuzz()
+            print(i, end=" ")
