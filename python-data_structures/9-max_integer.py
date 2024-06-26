cc78aa7fb1088825439e025fa6ed1095b34abff4
davidbirenzi <d.birenzi@alustudent.com>
2024-06-26 03:37:17 -0700

    maximum integer

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:  # Check if the list is empty
+    if not my_list:  
         return None
 
-    max_value = my_list[0]  # Initialize the maximum value with the first element
+    max_value = my_list[0]  
 
     for num in my_list:
         if num > max_value:
             max_value = num
+
     return max_value
