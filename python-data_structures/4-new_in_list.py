5fbc28dc9cf082422696c1de83203b6b5ae08871
davidbirenzi <d.birenzi@alustudent.com>
2024-06-26 02:56:32 -0700

    new in list

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
+
+    # Create a copy of the original list
+    new_list = my_list[:]
+
+    # Replace the element at the specified index
+    new_list[idx] = element
+
+    return new_list
