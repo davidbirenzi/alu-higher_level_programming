070887256e8476fac55caa3622ed261b0549b8cf
davidbirenzi <d.birenzi@alustudent.com>
2024-07-03 16:12:43 -0700

    print list integers

--- python-exceptions/2-main.py
+++ python-exceptions/2-main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-safe_print_list_integers = __import__('2-safe_print_list_integers').safe_print_list_integers
+safe_print_list_integers = \
+    __import__('2-safe_print_list_integers').safe_print_list_integers
 
 my_list = [1, 2, 3, 4, 5]
 
@@ -7,11 +8,8 @@ nb_print = safe_print_list_integers(my_list, 2)
 print("nb_print: {:d}".format(nb_print))
 
 my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-nb_print = safe_print_list_integers(my_list, 7)  # Using 7 directly instead of len(my_list)
+nb_print = safe_print_list_integers(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
 
-try:
-    nb_print = safe_print_list_integers(my_list, 9)  # Using 9 directly instead of len(my_list) + 2
-    print("nb_print: {:d}".format(nb_print))
-except IndexError:
-    print("Caught an IndexError as expected")
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
+#!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    printed_count = 0
+    count = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            printed_count += 1
-        except IndexError:
-            print()  # Ensure a new line is printed before raising the exception
-            raise
-        except (ValueError, TypeError):
+            count += 1
+        except (TypeError, ValueError):
             continue
-    print()  # Ensure a new line is printed after the loop
-    return printed_count
+    print()
+    return count
