d459145091feb92d5f98801f5a8d92cfe7f6cc06
davidbirenzi <d.birenzi@alustudent.com>
2024-07-08 12:56:32 -0700

    Simple rectangle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+
+
 class Rectangle:
+    """
+    Empty class representing a rectangle.
+    """
+
     def __init__(self):
+        """
+        Initializes an instance of the Rectangle class.
+        """
         pass
