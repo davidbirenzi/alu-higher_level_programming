0bca49302b8146cf59e3b3775326be29bdf4174e
davidbirenzi <d.birenzi@alustudent.com>
2024-07-24 11:05:21 +0000

    hbtn status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -11,8 +11,8 @@ if url.startswith('https://'):
     url = 'https://intranet.hbtn.io/status'
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
