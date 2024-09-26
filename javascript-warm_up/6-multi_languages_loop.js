96c4efe815785aa745437a40acbcb398c1a8fc49
davidbirenzi <d.birenzi@alustudent.com>
2024-09-26 13:52:51 +0000

    6 corrected

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-let lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-for(let line of lines){
-    console.log(line);
+const strs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const str of strs) {
+  console.log(str);
 }
